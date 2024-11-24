ee36ea3d41a22b952394fda94a2e879f0cea7d18
Sofia1653 <sofedu2005@gmail.com>
2024-11-24 14:53:55 -0300

    historias com a parte da main

--- jatoba/admin.py
+++ jatoba/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Tarefa
+from .models import Tarefa, Culturas, Rotacoes
 
+admin.site.register(Culturas)
+admin.site.register(Rotacoes)
 admin.site.register(Tarefa)
--- projeto13/urls.py
+++ projeto13/urls.py
@@ -40,5 +40,13 @@ urlpatterns = [
     path('rucula/', views.rucula_infos, name='rucula_infos'),    
     
     path('tomate/', views.tomate_infos, name='tomate_infos'),
+
+    path('fertilizante/', views.AddFertilizante.as_view(), name='fertilizante'),
+    path('fertilizantes/', views.VerFertilizante.as_view(), name='visualizar_fert'),
+    path('fertilizante/<int:id>/delete/', views.delete_fertilizante_view, name='delete_fert'),
+    path('semente/', views.AddSemente.as_view(), name='semente'),
+    path('sementes/', views.VerSemente.as_view(), name='visualizar_sem'),
+    path('semente/<int:id>/delete/', views.delete_semente_view, name='delete_sem'),
+    path('rotacao/', views.Rotacao.as_view(), name='rotacao'),
 ]
 
